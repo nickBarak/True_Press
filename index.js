b3202c0b1bbf0e57cf9a0a803a6bb17b3424b4df
nickBarak <nicholasjbarak@gmail.com>
2020-09-13 09:21:24 -0700

    .

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ app.prepare().then(_=> {
     server.enable('trust proxy');
     
     server.use( (req, res) => {
-        if (req.secure || dev) { handle(req, res, parse(req.url, true)) }
-        else return res.redirect('https://www.truepress.online');
+        handle(req, res, parse(req.url, true));
+        // if (req.secure || dev) { handle(req, res, parse(req.url, true)) }
+        // else return res.redirect('https://www.truepress.online');
     });
 
     server.listen(port, _=> console.log('Listening on port', port))
